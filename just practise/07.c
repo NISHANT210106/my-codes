0518d9ce80530419ef17a71ed4560b5e048794e7
NISHANT210106 <nishantyadav2207@gmail.com>
2024-10-02 20:38:41 +0530

    lates

--- just practise/07.c
+++ just practise/07.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
-int ser(int);
-int ser(int x){
-    int n;
-    if (x == 0){
-        return 0;
-    }
-    else if (x == 1){
-        return 1;
-    }
-    else{
-        return ser(x - 1) + ser(x - 2);
-    }
-}
+
 int main(){
-    int fib;
-    for (int i = 0; i < 12; i++){
-        fib = ser(i);
-        printf("%d\n", fib);
+    int num=1;
+    for (int i = 0; i < 5; i++){
+    for (int j = 0; j < i; j++)
+    {
+        printf("%d",num);
+            num++;
     }
+            printf("\n");
+}
     return 0;
 }
+
